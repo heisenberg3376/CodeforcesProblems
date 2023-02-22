48d0354a42f03aafdd56e016b259ff83ecd664c9
heisenberg3376 <kattaphanendrasairam@gmail.com>
2023-02-22 15:08:08 +0530

    again

---
+++ BeautifulMatrix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[6][6],r,c,i,j;
+    for(i=1;i<6;i++){
+        for(j=1;j<6;j++){
+            cin>>a[i][j];
+            if(a[i][j]==1){
+                r = abs(3-i);
+                c = abs(3-j);
+            }
+        }
+    }
+    cout<<(r+c);
+    return 0;
+}
